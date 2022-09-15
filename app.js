e7c96c75de8517d7c41b2c84b7f9829d3386afbf
rishisriv <48189716+rishisriv@users.noreply.github.com>
2022-09-15 19:01:12 -0400

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send("welcome to the home page baby")
+    res.send("I know EBS, AWS - new update")
 })
 const port = process.env.port || 3000 
 
