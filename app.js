ae1ee27642f323e259baa0e6b87f18fd3087b51a
rishisriv <48189716+rishisriv@users.noreply.github.com>
2022-09-15 18:52:56 -0400

    port variable

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express()
 app.get('/', (req, res)=>{
     res.send("welcome to the home page baby")
 })
+const port = process.env.port || 3000 
 
-app.listen(3000, () =>{
+app.listen(port, () =>{
     console.log('wazzappp')
 });
 
